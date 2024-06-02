b7bb90d6b035fa6ce434837058f2f7c0007548b7
M1nts02 <m1nts02@M1nts02deMacBook-Air.local>
2024-06-02 16:59:20 +0800

    Add lualine

---
+++ config/nvim/lua/configs/lualine.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+local opts = {
+  options = {
+    icons_enabled = true,
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = {
+      statusline = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = {
+      statusline = 3000,
+      tabline = 3000,
+      winbar = 3000,
+    },
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "filename", "aerial" },
+    lualine_c = {
+      "%=",
+      function()
+        return require("lsp-progress").progress()
+      end,
+    },
+    lualine_x = {
+      "diagnostics",
+      function()
+        local names = {}
+        for _, server in pairs(vim.lsp.get_active_clients { bufnr = 0 }) do
+          table.insert(names, server.name)
+        end
+        return table.concat(names, " ")
+      end,
+    },
+    lualine_y = { "branch" },
+    lualine_z = { "location", "progress" },
+  },
+}
+
+M.config = function()
+  require("lualine").setup(opts)
+end
+
+return M
