be2e6007af3a56ba66275ae6dbf83968673aff5d
M1nts02 <zyh1032712227@outlook.com>
2024-06-19 11:55:00 +0800

    Update fzf

--- config/nvim/lua/configs/fzf-lua.lua
+++ config/nvim/lua/configs/fzf-lua.lua
@@ -1,79 +1,16 @@
 local M = {}
 
 local utils = require "modules.utils"
-local icons = require "modules.icons"
 
 M.keys = {
-  {
-    "<Space>f<CR>",
-    function()
-      require("fzf-lua").builtin { prompt = icons.Search .. " " }
-    end,
-    desc = "Builtin",
-  },
-  {
-    "<Space>ff",
-    function()
-      require("fzf-lua").files()
-    end,
-    desc = "File",
-  },
-  {
-    "<Space>fo",
-    function()
-      require("fzf-lua").oldfiles()
-    end,
-    desc = "Oldfile",
-  },
-  {
-    "<Space>fb",
-    function()
-      require("fzf-lua").buffers()
-    end,
-    desc = "Buffers",
-  },
-  {
-    "<Space>f<Tab>",
-    function()
-      require("fzf-lua").tabs()
-    end,
-    desc = "Tabs",
-  },
-  {
-    "<Space>lS",
-    function()
-      require("fzf-lua").lsp_workspace_symbols { prompt = icons.Search .. " " }
-    end,
-    desc = "Symbol Workspace",
-  },
-  {
-    "<Space>fh",
-    function()
-      require("fzf-lua").help_tags {
-        prompt = "󰋖 ",
-        winopts = {
-          height = 0.5,
-          width = 0.5,
-          preview = { hidden = "hidden" },
-        },
-      }
-    end,
-    desc = "Help",
-  },
-  {
-    "<A-x>",
-    function()
-      require("fzf-lua").commands {
-        prompt = " ",
-        winopts = {
-          height = 0.5,
-          width = 0.5,
-          preview = { hidden = "hidden" },
-        },
-      }
-    end,
-    desc = "Commands",
-  },
+  { "<Space>f<CR>", "<CMD>FzfLua builtin<CR>", desc = "Builtin" },
+  { "<Space>ff", "<CMD>FzfLua files<CR>", desc = "File" },
+  { "<Space>fo", "<CMD>FzfLua oldfiles<CR>", desc = "Oldfile" },
+  { "<Space>fb", "<CMD>FzfLua buffers<CR>", desc = "Buffers" },
+  { "<Space>f<Tab>", "<CMD>FzfLua tabs<CR>", desc = "Tabs" },
+  { "<Space>lS", "<CMD>FzfLua lsp_workspace_symbols<CR>", desc = "Symbol Workspace" },
+  { "<Space>fh", "<CMD>FzfLua helptags<CR>", desc = "Help" },
+  { "<Space>fc", "<CMD>FzfLua commands<CR>", desc = "Commands" },
 }
 
 M.opts = {
@@ -106,52 +43,6 @@ M.opts = {
     ["header"] = { "fg", "Comment" },
     ["gutter"] = { "bg", "Normal" },
   },
-  previewers = {
-    builtin = {
-      treesitter = { enable = false },
-      extensions = {
-        ["png"] = { "viu", "-b" },
-        ["svg"] = { "chafa", "{file}" },
-        ["jpg"] = { "viu", "-b" },
-      },
-    },
-  },
-  files = { prompt = icons.Search .. " ", cwd_prompt = false },
-  git = {
-    files = {
-      prompt = icons.Search .. " ",
-      cmd = "git ls-files --exclude-standard",
-      multiprocess = true,
-      git_icons = true,
-      file_icons = true,
-      color_icons = true,
-      cwd_header = false,
-    },
-    status = { prompt = icons.Search .. " " },
-    commits = { prompt = icons.Search .. " " },
-    bcommits = { prompt = icons.Search .. " " },
-    branches = { prompt = icons.Search .. " " },
-    tags = { prompt = icons.Search .. " " },
-    stash = { prompt = icons.Search .. " " },
-  },
-  grep = { prompt = icons.WordFile .. " ", input_prompt = icons.Input .. " " },
-  args = { prompt = icons.Search .. " " },
-  oldfiles = { prompt = icons.DefaultFile .. " ", cwd_only = false },
-  buffers = { prompt = icons.Search .. " " },
-  tabs = { prompt = icons.Search .. " ", tab_title = "Tab", tab_marker = icons.Bookmarks .. " " },
-  lines = { prompt = icons.Search .. " " },
-  blines = { prompt = icons.Search .. " " },
-  tags = { prompt = icons.Search .. " ", no_header = true, no_header_i = true },
-  btags = { prompt = icons.Search .. " " },
-  colorschemes = { prompt = icons.Colorscheme .. " ", live_preview = false, winopts = { height = 0.55, width = 0.30 } },
-  awesome_colorschemes = { prompt = icons.Colorscheme .. " ", live_preview = false },
-  quickfix_stack = { prompt = icons.Search .. " ", marker = icons.Bookmarks .. " " },
-  lsp = {
-    prompt_postfix = icons.Search .. " ",
-    code_actions = { prompt = icons.Search .. " " },
-    finder = { prompt = icons.Search .. " " },
-  },
-  diagnostics = { prompt = icons.Search .. " " },
 }
 
 return M
