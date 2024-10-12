dbcafc39f7f719676fb26353d4262fb18b55e163
M1nts02 <zyh1032712227@outlook.com>
2024-10-12 14:04:31 +0800

    Delete aerospace and sketchybar

--- config/sketchybar/README.md
+++
@@ -1,8 +0,0 @@
-# Skeychybar
-
-## Install
-
-- `brew install --cask sf-symbols`
-- link into `~/.config/`
-- `chmod +x ~/.config/sketchybar/plugins/*`
-- `brew services start sketchybar`
--- config/sketchybar/items/battery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item battery right \
-    --set battery \
-    update_freq=20 \
-    label.font="$FONT:Normal:$FONT_SIZE"  \
-    icon.font="$ICON_FONT:Normal:$ICON_FONT_SIZE" \
-    script="$PLUGIN_DIR/battery.sh"
