4ed27798b03c1fbd9e9981880f4556632a2fa4e0
M1nts02 <zyh1032712227@outlook.com>
2024-10-22 18:49:19 +0800

    Update vim

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -8,13 +8,13 @@ return {
     end,
     dependencies = { "b0o/schemastore.nvim" },
   },
-  {
+  { -- hover
     "lewis6991/hover.nvim",
     event = { "LspAttach" },
     keys = require("plugins.lsp.hover").keys,
     config = require("plugins.lsp.hover").config,
   },
-  {
+  { -- outline
     "stevearc/aerial.nvim",
     cmd = require("plugins.lsp.aerial").cmd,
     keys = require("plugins.lsp.aerial").keys,
