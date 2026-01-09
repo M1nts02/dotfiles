0c6eb884d0f08b81a02719d68ff894b1dc0820bd
M1nts02 <zyh1032712227@outlook.com>
2026-01-10 05:29:46 +0800

    update font

--- hammerspoon/modules/menu/screenshot.lua
+++ hammerspoon/modules/menu/screenshot.lua
@@ -3,7 +3,7 @@ local singleKey = Menu.singleKey
 
 add("Screenshot", {
   { singleKey("2", "Clipboard"), actions["Screenshot Clipboard"].run },
-  { singleKey("3", "Screen"), actions["Screenshot Screen"].run },
+  --{ singleKey("3", "Screen"), actions["Screenshot Screen"].run },
   { singleKey("4", "Area"), actions["Screenshot Area"].run },
   { singleKey("5", "App"), actions["Screenshot App"].run },
   { singleKey("q", "Quit"), function() end },
